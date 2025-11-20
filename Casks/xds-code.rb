a37b40cbc4e694063e257d5bc52d786c5fbac9d9
Xavier Perri <contact@xdpxi.dev>
2025-11-20 12:39:50 +1100

    Fix casks and add x64 support

--- Casks/xds-code.rb
+++ Casks/xds-code.rb
@@ -1,20 +1,30 @@
 cask "xds-code" do
   version "0.6.2"
-  sha256 "9030f60bca60f77873771647b9757ddf4adc630fedc84b5bf65891c9e6658c5d"
 
-  url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_aarch64_macos.dmg",
-      verified: "github.com"
+  on_arm do
+    sha256 "9030f60bca60f77873771647b9757ddf4adc630fedc84b5bf65891c9e6658c5d"
+
+    url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_aarch64_macos.dmg",
+        verified: "github.com"
+  end
+
+  on_intel do
+    sha256 "84476c852da846fb4d00d2a01f80e041b8e9c30b03344bd4a9ef391c3e062f7e"
+
+    url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_x64_macos.dmg",
+        verified: "github.com"
+  end
+
   name "XD's Code"
   desc "XD's Code is a code editor that is insipred by VSC."
   homepage "https://github.com/XDPXI/XDs-Code"
 
   livecheck do
-    url "https://github.com/XDPXI/XDs-Code/releases"
+    url :url
     strategy :github_latest
-    end
   end
 
-  app "ads-code.app"
+  app "xds-code.app"
 
   zap trash: [
     "~/Library/Preferences/xds-code.plist",
