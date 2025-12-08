73ee3c552a92afe5b49df3a32c2a6e8baf1e1ced
Xavier Perri <contact@xdpxi.dev>
2025-12-09 08:30:05 +1100

    Create darkwaters.rb

---
+++ Casks/darkwaters.rb
@@ -0,0 +1,23 @@
+cask "darkwaters" do
+  version "1.0.0b"
+  sha256 "8101c417d4accd124e33ca58019532505f84b76fa5dc712159c50dc074c1dee9"
+
+  url "http://cdn.xdpxi.net:8716/darkwaters/version/#{version}/Darkwaters-MacOS-#{version}.zip",
+      verified: "cdn.xdpxi.net"
+  name "Darkwaters"
+  desc "A top-down Sea of Theives inspired game."
+  homepage "https://xdpxi.itch.io/mono/"
+
+  livecheck do
+    url "http://cdn.xdpxi.net:8716/browse/darkwaters/version/"
+    strategy :page_match do |page|
+      page.scan(%r{<a href="/browse/darkwaters/version/([^/]+)/"}i).map(&:first)
+    end
+  end
+
+  app "Darkwaters.app"
+
+  zap trash: [
+    "~/Library/Application Support/darkwaters",
+  ]
+end
