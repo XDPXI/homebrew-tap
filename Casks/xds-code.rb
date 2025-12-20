e1149f71392326e1c6b3e741f7e37c235a1e6959
Xavier Perri <contact@xdpxi.dev>
2025-12-21 00:21:53 +1100

    Bump version to 0.7.0 and update checksums
    Updated version, SHA256 checksums, and description text.

--- Casks/xds-code.rb
+++ Casks/xds-code.rb
@@ -1,22 +1,22 @@
 cask "xds-code" do
-  version "0.6.3"
+  version "0.7.0"
 
   on_arm do
-    sha256 "99FF26C940AE2B5229F0AE8FA5893EC463C79FFD389031A0ABC670B4747636DB"
+    sha256 "7B91A32E78614EE9682A1DB7D7DD11EC3F2F60FEE9C950BE83895B4CF5A1A1E1"
 
     url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_macos-aarch64.dmg",
         verified: "github.com"
   end
 
   on_intel do
-    sha256 "D71011B8FA74AC74BBBC7BC8531BC4E68CDB798D0459FF1CB9239F2C018A343C"
+    sha256 "C7EFCA2F65241EC21945B58B7CAD9C6DDDE20CE0E2A4D220E0925E8B80A6301E"
 
     url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_macos-x64.dmg",
         verified: "github.com"
   end
 
   name "XD's Code"
-  desc "XD's Code is a code editor that is insipred by VSC."
+  desc "XD's Code is a code editor that is inspired by VSC."
   homepage "https://github.com/XDPXI/XDs-Code"
 
   livecheck do
