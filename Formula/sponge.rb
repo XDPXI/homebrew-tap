3aaa560e2a877da8991678f071dba221002ba04d
Xavier Perri <contact@xdpxi.dev>
2026-01-04 19:03:10 +1100

    Update sponge.rb

--- Formula/sponge.rb
+++ Formula/sponge.rb
@@ -2,22 +2,21 @@ class Sponge < Formula
   desc "A performant programming language"
   homepage "https://sponge.xdpxi.dev"
   license "MIT"
-  version "1.0.6"
+  version "1.0.7"
 
   depends_on "sdl2"
-  conflicts_with "spongedev"
 
   on_macos do
     on_intel do
       url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-macos-x86_64.tar.gz",
           verified: "cdn.xdpxi.net"
-      sha256 "224E4B1A050760CAED3F499F3DE988CD82CC6CD8270E7F03309229B3F04E3D3F"
+      sha256 "5F73C4C354CA999D90CF60BD546EF7B9469BB6EA66F08B52376AB052E340173F"
     end
 
     on_arm do
       url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-macos-aarch64.tar.gz",
           verified: "cdn.xdpxi.net"
-      sha256 "7267D592910A1505858988B5DA862D6591240A1084366BC2F4664D67FB09FBE6"
+      sha256 "F109E5BF4B38A1A4A6EB21BA5D2C523088F8FABEF695CAED3044894BB17F9043"
     end
   end
 
@@ -25,7 +24,7 @@ class Sponge < Formula
     on_intel do
       url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-linux-x86_64.tar.gz",
           verified: "cdn.xdpxi.net"
-      sha256 "DA4C0D2D95CBA0219D4BFBC380C863558F458817493B6ADB25472D306DA17FF8"
+      sha256 "FE8106773B2092FAC81632E06D0EF2C34C091AAAF06AE73B091E017B1086DEEB"
     end
   end
 
