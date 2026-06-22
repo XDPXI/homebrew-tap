cf471dbdd933d817ec07caafb421d5928146c2af
Xavier Perri <contact@xdpxi.dev>
2026-06-22 17:40:03 +1000

    Fix cdn url

--- Casks/mono.rb
+++ Casks/mono.rb
@@ -2,14 +2,14 @@ cask "mono" do
   version "1.5.0b"
   sha256 "2b9dda7a2a9f1ebc9749cb0118e04e1dc267b283318126ecd1dbcee43119d8d6"
 
-  url "http://cdn.xdpxi.net:8716/mono/version/#{version}/Mono-MacOS-#{version}.zip",
+  url "http://cdn.xdpxi.net/mono/version/#{version}/Mono-MacOS-#{version}.zip",
       verified: "cdn.xdpxi.net"
   name "Mono"
   desc "Monochrome-styled 2D platformer"
   homepage "https://xdpxi.itch.io/mono/"
 
   livecheck do
-    url "http://cdn.xdpxi.net:8716/browse/mono/version/"
+    url "http://cdn.xdpxi.net/browse/mono/version/"
     strategy :page_match do |page|
       page.scan(%r{<a href="/browse/mono/version/([^/]+)/"}i).map(&:first)
     end
--- Formula/sponge.rb
+++ Formula/sponge.rb
@@ -6,13 +6,13 @@ class Sponge < Formula
 
   on_macos do
     on_intel do
-      url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-macos-x86_64.tar.gz",
+      url "http://cdn.xdpxi.net/sponge/#{version}/sponge-#{version}-macos-x86_64.tar.gz",
           verified: "cdn.xdpxi.net"
       sha256 "929299A971A42EE7599FD0F0BFCAD7E2A5AAA268F48185B86B48EDDF2F364287"
     end
 
     on_arm do
-      url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-macos-aarch64.tar.gz",
+      url "http://cdn.xdpxi.net/sponge/#{version}/sponge-#{version}-macos-aarch64.tar.gz",
           verified: "cdn.xdpxi.net"
       sha256 "6ECA94C01AAAF5BC2E46E7D877966E0C93A08E2292EDCA6290001AD6202F42AC"
     end
@@ -20,7 +20,7 @@ class Sponge < Formula
 
   on_linux do
     on_intel do
-      url "http://cdn.xdpxi.net:8716/sponge/#{version}/sponge-#{version}-linux-x86_64.tar.gz",
+      url "http://cdn.xdpxi.net/sponge/#{version}/sponge-#{version}-linux-x86_64.tar.gz",
           verified: "cdn.xdpxi.net"
       sha256 "C090DC7EBBB583ED9252E626E80527BA10554231964586CC1E5DFE1AB0510728"
     end
