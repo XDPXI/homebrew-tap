e39e7069801d4c69eb2d26b3c230f23de5e6c89f
Xavier Perri <contact@xdpxi.dev>
2026-07-01 21:30:27 +1000

    Fixes

--- Casks/gin.rb
+++ Casks/gin.rb
@@ -1,17 +1,17 @@
 cask "gin" do
-  version "3.0.1"
-
-  sha256 "05D1A30A2A1F5ACED3CEC4FE22315B3A6F220F4AFD6ABAA58ED6BF9501133765"
-
-  url "https://git.xdpxi.net/xdpxi/Gin/releases/download/#{version}/Gin.zip",
-      verified: "git.xdpxi.net"
   name "Gin"
   desc "A modern Wine wrapper for macOS built with SwiftUI"
   homepage "https://git.xdpxi.net/xdpxi/Gin"
+  licence "GPL-3.0-only"
+  version "3.0.1"
 
-  depends_on formula: "gincmd"
+  url "https://git.xdpxi.net/xdpxi/Gin/releases/download/#{version}/Gin.zip",
+      verified: "git.xdpxi.net"
+  sha256 "05D1A30A2A1F5ACED3CEC4FE22315B3A6F220F4AFD6ABAA58ED6BF9501133765"
 
   app "Gin.app"
+
+  depends_on formula: "gincmd"
 
   postflight do
     system_command "/usr/bin/xattr",
--- Formula/gincmd.rb
+++ Formula/gincmd.rb
@@ -1,10 +1,14 @@
 class Gincmd < Formula
   desc "A modern Wine wrapper for macOS built with SwiftUI"
   homepage "https://git.xdpxi.net/xdpxi/Gin"
-  license "GNU GENERAL PUBLIC LICENSE v3"
+  license "GPL-3.0-only"
   version "3.0.1"
 
   url "https://git.xdpxi.net/xdpxi/Gin/releases/download/#{version}/GinCmd.zip",
-    verified: "git.xdpxi.net"
+      verified: "git.xdpxi.net"
   sha256 "23439E598AE7BF95192CACDC08AB64C6DAD9F976F51B6D79BC7D17F4AC4247B9"
+
+  def install
+    bin.install "GinCmd"
+  end
 end
