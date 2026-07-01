12b633235abdaeb1af4047eb40f795b4a2cf5717
Xavier Perri <contact@xdpxi.dev>
2026-07-02 01:20:20 +1000

    Update gin from 3.0.2 to 3.0.3

--- Casks/gin.rb
+++ Casks/gin.rb
@@ -2,11 +2,11 @@ cask "gin" do
   name "Gin"
   desc "A modern Wine wrapper for macOS built with SwiftUI"
   homepage "https://git.xdpxi.net/xdpxi/Gin"
-  version "3.0.2"
+  version "3.0.3"
 
   url "https://git.xdpxi.net/xdpxi/Gin/releases/download/#{version}/Gin.zip",
       verified: "git.xdpxi.net"
-  sha256 "38e107edbb3b3dc8ad1890efe8314822a5e0ff3b2826c6d283414bd50b5f8963"
+  sha256 "d0ef550a26976ef93213fdde1288f46c33714816ca8d8da1a84b4cb50cf05984"
 
   app "Gin.app"
 
--- Formula/gincmd.rb
+++ Formula/gincmd.rb
@@ -2,11 +2,11 @@ class Gincmd < Formula
   desc "A modern Wine wrapper for macOS built with SwiftUI"
   homepage "https://git.xdpxi.net/xdpxi/Gin"
   license "GPL-3.0-only"
-  version "3.0.2"
+  version "3.0.3"
 
   url "https://git.xdpxi.net/xdpxi/Gin/releases/download/#{version}/GinCmd.zip",
       verified: "git.xdpxi.net"
-  sha256 "bcca0f5ba43defa2b1b6f2a53a3a151aaa89b6f4bc245aea1e3b4aced6be5990"
+  sha256 "65a1b8349929e863ec2773678f8c4c971c99f68e986438a5c98139c744c633f9"
 
   def install
     bin.install "GinCmd"
