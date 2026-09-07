7336f2873e6360d153911a7bda3d0b798f1c4882
Xavier Perri <contact@xdpxi.dev>
2026-09-08 09:33:51 +1000

    Update xds-code version and SHA256 checksums

--- Casks/xds-code.rb
+++ Casks/xds-code.rb
@@ -1,15 +1,15 @@
 cask "xds-code" do
-  version "0.8.0"
+  version "0.9.4"
 
   on_arm do
-    sha256 "5f21e1f3cae2a71f5e0af1c6bfa3607e4dbe991a74d728ede39836f5ccbb674b"
+    sha256 "37eda62a5d80996331b10566b7a59ecc2e48eee9070204ea7393251450aa1efe"
 
     url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_macos-aarch64.dmg",
         verified: "github.com"
   end
 
   on_intel do
-    sha256 "9f2d1110670296753700ac2d82ee4441ceff91e8fa40050516e9dec0dc63dd0e"
+    sha256 "dcb994ea4714e15a9750a22e21aba2b8f7c6ea9d0ca04fc16f67bea78b4a709d"
 
     url "https://github.com/XDPXI/XDs-Code/releases/download/#{version}/xds-code_#{version}_macos-x64.dmg",
         verified: "github.com"
